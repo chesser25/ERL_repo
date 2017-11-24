5356370bb7a32ec0cf0fc1755badd9a9700cb430
chesser25 <andrewchess25@gmail.com>
2017-11-24 03:19:10 +0200

    Validation was added for register screen

--- ERL/ERL/Controllers/HomeController.cs
+++ ERL/ERL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ERL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,16 @@ namespace ERL.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult  Index([Bind(Include = "UserName, Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View();
         }
     }
