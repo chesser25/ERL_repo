c750601234b8ab951a05b8c22b8b54f89f048f04
chesser25 <andrewchess25@gmail.com>
2017-11-24 02:13:44 +0200

    Controller to manage info page was added

---
+++ ERL/ERL/Controllers/InfoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERL.Models;
+
+namespace ERL.Controllers
+{
+    public class InfoController : Controller
+    {
+        private UsersDBEntities1 db = new UsersDBEntities1();    
+
+        // GET: Info/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: Info/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,CompanyName,BirthDate,Gender,Position,Department,EmployedFrom,EmployedTo,LeaveReason,AddressLine1,AddressLine2,ZIP,Place,CountryCode")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userInfo);
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
